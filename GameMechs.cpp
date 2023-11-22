92c13ae13fbdbefc6c82bdb0f66e4929b0018d5f
Yuchen Tao <raphael@yuchendembp.home>
2023-11-22 18:15:12 -0500

    message

--- GameMechs.cpp
+++ GameMechs.cpp
@@ -2,12 +2,18 @@
 
 GameMechs::GameMechs()
 {
-
+    input = 0;
+    exitFlag = false;
+    boardSizeX = 30;
+    boardSizeY = 15;
 }
 
 GameMechs::GameMechs(int boardX, int boardY)
 {
-
+    input = 0;
+    exitFlag = false;
+    boardSizeX = boardX;
+    boardSizeY = boardY;
 }
 
 // do you need a destructor?
@@ -16,38 +22,50 @@ GameMechs::GameMechs(int boardX, int boardY)
 
 bool GameMechs::getExitFlagStatus()
 {
-
+    return exitFlag;
 }
 
 char GameMechs::getInput()
-{
-
+{   
+    if(MacUILib_hasChar())
+    {
+        input = MacUILib_getChar();
+    }
+    return input;
 }
 
 int GameMechs::getBoardSizeX()
 {
-
+    return boardSizeX;
 }
 
 int GameMechs::getBoardSizeY()
 {
-
+    return boardSizeY;
 }
 
 
 void GameMechs::setExitTrue()
 {
-
+    exitFlag = true;
 }
 
 void GameMechs::setInput(char this_input)
 {
-
+    input = this_input;
 }
 
 void GameMechs::clearInput()
 {
-
+    input = 0;
 }
 
+int GameMechs::getScore()
+{
+    return score;
+}
 
+void GameMechs::incrementScore()
+{
+    score++;
+} 
